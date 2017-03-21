f16cd1b6b62ef4a0b4704d6386d237913d15f3c6
Robbie Brandrick <me@robbiebrandrick.com>
2017-03-21 07:17:28 -0400

    Initial Commit. Used react to display a simple message in the browser

---
+++ ReactJSDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div class="commentBox">
+                Hello, world! I am a CommentBox.
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
